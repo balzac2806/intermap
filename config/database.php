fa212d6dab31263d4a078eeab074dc4c7d95e379
Daniel Golubiewski <balzac2806@gmail.com>
2016-04-25 09:19:45 +0200

    DB STG

--- config/database.php
+++ config/database.php
@@ -60,13 +60,26 @@ return [
             'strict' => false,
             'engine' => null,
         ],
+        // LOCAL
+//        'pgsql' => [
+//            'driver' => 'pgsql',
+//            'host' => env('DB_HOST', 'localhost'),
+//            'port' => env('DB_PORT', '5432'),
+//            'database' => env('DB_DATABASE', 'intermap'),
+//            'username' => env('DB_USERNAME', 'homestead'),
+//            'password' => env('DB_PASSWORD', 'secret'),
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//            'schema' => 'public',
+//        ],
+        // STG
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'intermap'),
-            'username' => env('DB_USERNAME', 'homestead'),
-            'password' => env('DB_PASSWORD', 'secret'),
+            'username' => env('DB_USERNAME', 'adminqxdg1bw'),
+            'password' => env('DB_PASSWORD', '_sdVKbj9UF6G'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
