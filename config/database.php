1e4a4f0104867bfc1820e11f49d8a385826665af
Daniel Golubiewski <balzac2806@gmail.com>
2016-04-25 10:36:17 +0200

    Local Database

--- config/database.php
+++ config/database.php
@@ -61,29 +61,29 @@ return [
             'engine' => null,
         ],
         // LOCAL
-//        'pgsql' => [
-//            'driver' => 'pgsql',
-//            'host' => env('DB_HOST', 'localhost'),
-//            'port' => env('DB_PORT', '5432'),
-//            'database' => env('DB_DATABASE', 'intermap'),
-//            'username' => env('DB_USERNAME', 'homestead'),
-//            'password' => env('DB_PASSWORD', 'secret'),
-//            'charset' => 'utf8',
-//            'prefix' => '',
-//            'schema' => 'public',
-//        ],
-        // STG
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'intermap'),
-            'username' => env('DB_USERNAME', 'adminqxdg1bw'),
-            'password' => env('DB_PASSWORD', '_sdVKbj9UF6G'),
+            'username' => env('DB_USERNAME', 'homestead'),
+            'password' => env('DB_PASSWORD', 'secret'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
         ],
+        // STG
+//        'pgsql' => [
+//            'driver' => 'pgsql',
+//            'host' => env('DB_HOST', 'localhost'),
+//            'port' => env('DB_PORT', '5432'),
+//            'database' => env('DB_DATABASE', 'intermap'),
+//            'username' => env('DB_USERNAME', 'adminqxdg1bw'),
+//            'password' => env('DB_PASSWORD', '_sdVKbj9UF6G'),
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//            'schema' => 'public',
+//        ],
     ],
     /*
       |--------------------------------------------------------------------------
